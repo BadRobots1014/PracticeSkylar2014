2a9166f0ede67120be44a55aefb89248a5c5ee68
singhalmanu9 <singhalmanu9@gmail.com>
2014-01-21 18:14:48 -0500

    added BadSubsystem and Logger

---
+++ src/com/practiceRobot/interfaces/Logger.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.practiceRobot.interfaces;
+
+/**
+ *
+ * @author rsinghal
+ */
+public interface Logger {
+    
+    public void log(String out);
+    
+}

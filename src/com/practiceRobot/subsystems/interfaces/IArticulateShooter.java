b1bf1bdfe184d68585c5c293faf4ef955fe490eb
singhalmanu9 <singhalmanu9@gmail.com>
2014-01-27 20:31:15 -0500

    began working on IShooter and IArticulateShooter

---
+++ src/com/practiceRobot/subsystems/interfaces/IArticulateShooter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.practiceRobot.subsystems.interfaces;
+
+/**
+ *
+ * @author rsinghal
+ */
+public interface IArticulateShooter 
+{
+    public void raiseShooter();
+    
+    public void lowerShooter();
+    
+}

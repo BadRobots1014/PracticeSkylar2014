b3d3ec08422a8436099de957c3bed95376731911
singhalmanu9 <singhalmanu9@gmail.com>
2014-01-31 16:19:26 -0500

    added a bunch of comments

--- src/com/practiceRobot/subsystems/interfaces/IArticulateShooter.java
+++ src/com/practiceRobot/subsystems/interfaces/IArticulateShooter.java
@@ -5,15 +5,27 @@
 package com.practiceRobot.subsystems.interfaces;
 
 /**
- *
- * @author rsinghal
+ * The interface for the ArticulateShooter subsystem. 
+ * @author Manu S.
  */
 public interface IArticulateShooter 
 {
+    /**
+     * Angles the shooter up.
+     */
     public void raiseShooter();
     
+    /**
+     * Angles the shooter down.
+     */
     public void lowerShooter();
     
+    /**
+     * Specifies the speed that the shooter
+     * should move at.
+     * @param speed the speed between -1.0 and 1.0
+     * negative for down and positive for up
+     */
     public void moveShooter(double speed);
     
 }
--- src/com/practiceRobot/subsystems/interfaces/IShooter.java
+++ src/com/practiceRobot/subsystems/interfaces/IShooter.java
@@ -5,10 +5,16 @@
 package com.practiceRobot.subsystems.interfaces;
 
 /**
- *
+ * The interface for the Shooter subsystem.
  * @author Manu S.
  */
 public interface IShooter 
 {
+    /**
+     * Moves the shooter at the specified speed.
+     * @param speed the speed we want the
+     * Shooter to shoot the frisbees at (between
+     * -1.0 and 1.0)
+     */
     public void setSpeed(double speed);
 }
